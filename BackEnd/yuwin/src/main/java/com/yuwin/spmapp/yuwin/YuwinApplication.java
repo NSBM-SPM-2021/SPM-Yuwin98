6a44b89081a42fd3a054a3a33dd96fc107ee5381
Yuwin Sapumana <yuwinsapumana212@gmail.com>
2021-08-05 13:27:52 +0530

    Initialized an Angular Front-End and a Spring Boot Back-End

---
+++ BackEnd/yuwin/src/main/java/com/yuwin/spmapp/yuwin/YuwinApplication.java
@@ -0,0 +1,13 @@
+package com.yuwin.spmapp.yuwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuwinApplication.class, args);
+	}
+
+}
