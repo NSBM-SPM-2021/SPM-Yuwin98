7113339515302f301559a12da7d4fd156808cec4
Yuwin Sapumana <yuwinsapumana212@gmail.com>
2021-08-22 13:17:18 +0530

    created home module

--- FrontEnd/SPM-APP/src/app/app.module.ts
+++ FrontEnd/SPM-APP/src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AuthModule } from './auth/auth.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  imports: [BrowserModule, AppRoutingModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
